58f3a847e074f9995293699beef1d33e82b7e159
morganic <morganic111@gmail.com>
2023-11-17 12:40:22 -0800

    abcdefg

--- new.lua
+++ new.lua
@@ -21,7 +21,6 @@ local maxOperatingTemp = 700
 local minOperatingTemp = 600
 
 local averageReactorTemp = 0
-
 local foundRods = {}
 
 --Control rods (CONTROL)
@@ -38,6 +37,95 @@ local fuelRodDepletion = {}
 local fuelRodCoreTemp = {}
 local fuelRodSkinTemp = {}
 local fuelRodColumnTemp = {}
-
+local averageFuelDepleation = 0
+local averageCoreTemp = 0
+local averageFuelColumnTemp = 0
 
 --Boilers (BOILER)
+
+local boilerColumns = {}
+local boilerWater = {}
+local boilerSteam = {}
+local boilerTemp = {}
+local averageBoilerTemp = 0
+local averageBoilerWater = 0
+local averageBoilerSteam = 0
+
+-- functions
+
+function averageTable(tableIN)
+    local sum = 0
+    for i, v in ipairs(tableIN) do
+        sum = sum + v
+    end
+    local average = sum / #tableIN
+    return average
+end
+
+function averageDouble(tableIN)
+    local sum = 0
+    for i, v in ipairs(tableIN) do
+        sum = sum + v
+    end
+    local average = sum / #tableIN
+    average = average * 100
+end
+
+
+--end ini
+
+scriptPhase = "Startup"
+
+for x=0,15 do
+    for y=0,15 do
+        local data = rbmk.getColumnData(x,y)
+        table.insert(foundRods, {x = x, y = y})
+        if data.type == "CONTROL" then
+            table.insert(controlRodColumns, {x = x, y = y})
+        elseif data.type == "FUEL" then
+            table.insert(fuelRodColumns, {x = x, y = y})
+        elseif data.type = "BOILER" then
+            table.insert(boilerColumns, {x = x, y = y})
+        else
+
+        end
+    end
+end
+
+scriptPhase = "main-prosssesing-loop"
+while true do
+    for i, column in ipairs(controlRodColumns) do
+        local x = column.x
+        local y = column.y
+        local data = rbmk.getColumnData(x,y)
+        table.insert(controlRodInserts, data.level)
+        table.insert(controlRodTemps, data.hullTemp)
+    end
+    for i, column in ipairs(fuelRodColumns) do
+        local x = column.x
+        local y = column.y
+        local data = rbmk.getColumnData(x,y)
+        table.insert(fuelRodDepletion, data.coreTemp)
+        table.insert(fuelRodCoreTemp, data.enrichment)
+        table.insert(fuelRodSkinTemp, data.coreSkinTemp)
+        table.insert(fuelRodColumnTemp, data.hullTemp)
+    end
+    for i, column in ipairs(boilerColumns) do
+        local x = column.x
+        local y = column.y
+        local data = rbmk.getColumnData(x,y)
+        table.insert(boilerWater, data.water)
+        table.insert(boilerSteam, data.steam)
+        table.insert(boilerTemp, data.hullTemp)
+    end
+
+    AvfuelCoreTemp = averageTable(fuelCoreHeat)
+    AvfuelColumnTemp = averageTable(fuelColunmHeat)
+    AvfuelColumnTemp = averageTable(fuelColunmHeat)
+    AvBoilerColunmTemp = averageTable(boilersHeat)
+
+    gpu.set(10,10, AvfuelCoreTemp) 
+    gpu.set(20,20, AvfuelColumnTemp)
+    gpu.set(30,30, AvfuelCoreTemp)
+    gpu.set(40,40, fuelColunmHeat)
+end
