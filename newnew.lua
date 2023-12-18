916139506037e4074d16c105ad6aa8d070f67b37
morganic <morganic111@gmail.com>
2023-12-18 14:14:20 -0800

    back at it again

---
+++ newnew.lua
@@ -0,0 +1,21 @@
+-- Librares 
+local component = require("component")
+local event = require("event")
+local term = require("term")
+local os = require("os")
+
+-- Objects
+local gpu = component.gpu
+local rbmk = component.rbmk_console
+local rs = {} -- reserved for later
+
+-- Scriptwide variables
+
+local scriptVer = "0.0.1 - A"
+local scriptPhase = ""
+
+    -- Values
+
+    -- Tables
+
+-- Functions
